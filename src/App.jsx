14375191f229139de0137a2c265aafef0c63e6aa
GRahul108 <rahulchand810@gmail.com>
2024-07-03 11:14:58 -0500

    Project structure setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count,setCount]=useState(0)
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
